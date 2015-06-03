ba8b1a8c9ce0cf77beaee6688745b3a2fb69915b
felix <felix@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-06-03 08:43:04 +0000

    Datenstruktur erweitert.

---
+++ trunk/ver1/src/FileSystem/FileSystemInterface.java
@@ -0,0 +1,7 @@
+package FileSystem;
+
+/**
+ * Created by felix on 03.06.15.
+ */
+public interface FileSystemInterface {
+}
---
+++ trunk/ver1/src/FileSystem/LocalFile.java
@@ -0,0 +1,20 @@
+package FileSystem;
+
+/**
+ * A LocalFile represents a File in the local filesystem, which
+ * is a leaf in our filesystem.
+ *
+ * Created by felix on 03.06.15.
+ */
+public class LocalFile extends File {
+
+
+    /**
+     * Creates a new LocalFile.
+     *
+     * @param name name of the new {@link File}.
+     */
+    public LocalFile(String name) {
+        super(name);
+    }
+}
