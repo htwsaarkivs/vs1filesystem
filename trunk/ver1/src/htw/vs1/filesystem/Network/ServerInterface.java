3cc1ff625be7a65421d21bcc36cc11cb555abed2
markus <markus@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-06-05 16:06:51 +0000

    Added Network-Package. Created a scrapyard, containing cowboy-Examples..

---
+++ trunk/ver1/src/htw/vs1/filesystem/Network/ServerInterface.java
@@ -0,0 +1,7 @@
+package htw.vs1.filesystem.Network;
+
+/**
+ * Created by markus on 05.06.15.
+ */
+public interface ServerInterface {
+}
---
+++ trunk/ver1/src/htw/vs1/filesystem/Network/TCPSSimpleServer.java
@@ -0,0 +1,8 @@
+package htw.vs1.filesystem.Network;
+
+/**
+ * Created by markus on 05.06.15.
+ */
+public class TCPSSimpleServer implements ServerInterface {
+
+}
