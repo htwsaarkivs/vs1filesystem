8f4fced09191519379a64d9c79329fa125e84d19
felixble <felix@blechschmitt.name>
2015-06-09 14:58:40 +0000

    NioTrial - iterate recursively through a directory.

--- src/htw/vs1/filesystem/Trials/NioTrial.java
+++ src/htw/vs1/filesystem/Trials/NioTrial.java
@@ -14,18 +14,31 @@ import java.util.List;
 public class NioTrial {
 
     public static void main(String[] args) throws Exception {
-        List<String> fileNames = new ArrayList<>();
-        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get("/Users/felix/htw"))) {
-            for (Path path : directoryStream) {
-                fileNames.add(path.toString());
-            }
-        } catch (IOException ex) {}
 
-
-        for (String fileName : fileNames) {
-            System.out.println(fileName + "\n");
+        if (args.length != 1) {
+            System.out.println("Usage: NioTrial <path-to-directory>");
+            System.exit(0);
         }
 
+        fileSystemTreeRecursion(Files.newDirectoryStream(Paths.get(args[0])), 0);
+    }
+
+    private static void fileSystemTreeRecursion(DirectoryStream<Path> directoryStream, int iterDepth) throws IOException {
+        for (Path path : directoryStream) {
+            System.out.println(getTab(iterDepth) + path.toAbsolutePath().toString());
+
+            if (path.toFile().isDirectory()) {
+                fileSystemTreeRecursion(Files.newDirectoryStream(Paths.get(path.toAbsolutePath().toString())), iterDepth+1);
+            }
+        }
+    }
+
+    private static String getTab(int depth) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            builder.append("\t");
+        }
+        return builder.toString();
     }
 
 }
