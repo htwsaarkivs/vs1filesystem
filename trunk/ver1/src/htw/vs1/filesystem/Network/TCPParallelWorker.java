0f1bd91920a278eb9b88220d64cdf678b5c50bb4
markus <markus@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-06-09 22:01:53 +0000

    Kleinere Änderungen an Servern

--- trunk/ver1/src/htw/vs1/filesystem/Network/TCPParallelWorker.java
+++ trunk/ver1/src/htw/vs1/filesystem/Network/TCPParallelWorker.java
@@ -1,7 +1,39 @@
 package htw.vs1.filesystem.Network;
 
+import java.net.Socket;
+
 /**
  * Created by markus on 07.06.15.
  */
-public class TCPParallelWorker {
+public class TCPParallelWorker extends Thread {
+
+
+    private Socket socket;
+
+    public TCPParallelWorker(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        System.out.println("Thread gestartet");
+        try {
+
+            socket.getOutputStream().write(new byte[] { (byte) 0x41, 0x0 });
+            System.out.println("IP: "+ socket.getInetAddress());
+            System.out.println("Port:" + socket.getPort());
+            //Thread.sleep(10000);
+
+            socket.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+
+
 }
