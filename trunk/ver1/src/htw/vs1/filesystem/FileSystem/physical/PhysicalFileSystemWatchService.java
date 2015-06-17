03ad026540d2cfcaf87992f72e29c6805a5ae284
felix <felix@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-06-17 16:05:38 +0000

    PhysicalFileSystemWatchService connected with virtual file system.

--- trunk/ver1/src/htw/vs1/filesystem/FileSystem/physical/PhysicalFileSystemWatchService.java
+++ trunk/ver1/src/htw/vs1/filesystem/FileSystem/physical/PhysicalFileSystemWatchService.java
@@ -1,5 +1,7 @@
 package htw.vs1.filesystem.FileSystem.physical;
 
+import htw.vs1.filesystem.FileSystem.virtual.FileSystem;
+import htw.vs1.filesystem.FileSystem.virtual.FileSystemInterface;
 import htw.vs1.filesystem.FileSystem.virtual.LocalFolder;
 
 import java.io.IOException;
@@ -9,16 +11,22 @@ import java.nio.file.Path;
  * Created by felix on 17.06.15.
  */
 public class PhysicalFileSystemWatchService extends AbstractWatchService {
+
+    private FileSystemInterface fileSystem;
+
     /**
      * Creates a WatchService and registers the given directory
      */
     PhysicalFileSystemWatchService() throws IOException {
         super(LocalFolder.getRootFolder().getPath(), true);
+
+        fileSystem = new FileSystem();
     }
 
     @Override
     protected void onEntryDelete(Path child, Path dir) {
         System.out.format("Deleted file/folder %s in directory %s\n", child.toFile().getName(), dir);
+        System.out.format("Relative path from local-root: " + LocalFolder.getRootFolder().getPath().relativize(child));
     }
 
     @Override
