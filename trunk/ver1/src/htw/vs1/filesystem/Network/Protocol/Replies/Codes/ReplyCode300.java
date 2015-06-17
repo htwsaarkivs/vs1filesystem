1904e4b2cc5a29d87f09877e8f617adc4bbcc8bb
markus <markus@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-06-17 09:27:36 +0000

    Added 300 ReplyCode

--- trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Replies/Codes/ReplyCode300.java
+++ trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Replies/Codes/ReplyCode300.java
@@ -1,5 +1,7 @@
 package htw.vs1.filesystem.Network.Protocol.Replies.Codes;
 
+import htw.vs1.filesystem.Network.Protocol.Replies.SimpleProtocolReply;
+import htw.vs1.filesystem.Network.Protocol.Replies.Type.SimpleProtocolReplyType;
 import htw.vs1.filesystem.Network.Protocol.Replies.Type.Type;
 
 /**
@@ -8,18 +10,28 @@ import htw.vs1.filesystem.Network.Protocol.Replies.Type.Type;
 public class ReplyCode300 extends ReplyCode {
 
 
+    public static final Type REPLY_TYPE = SimpleProtocolReplyType.PROMPT;
+    public static final int CODE = 300;
+    public static final String DESCRIPTION = "";
+    public static final String STANDARD_MESSAGE = "LOGIN REQUIRED";
+
+
+    public ReplyCode300(String user) {
+        this.message = STANDARD_MESSAGE + " FOR USER " + user;
+    }
+
     @Override
     public int getCode() {
-        return 0;
+        return CODE;
     }
 
     @Override
     public String getDescription() {
-        return null;
+        return DESCRIPTION;
     }
 
     @Override
     public Type getReplyType() {
-        return null;
+        return REPLY_TYPE;
     }
 }
