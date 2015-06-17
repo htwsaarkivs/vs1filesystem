a61bc6151129a880015b48f070c349034b4af48c
markus <markus@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-06-17 10:57:31 +0000

    LS implementiert TCPParallel Server manuell refactort oder wie auch immer man das schreibt.

--- trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Commands/LS.java
+++ trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Commands/LS.java
@@ -1,6 +1,7 @@
 package htw.vs1.filesystem.Network.Protocol.Commands;
 
 import htw.vs1.filesystem.Network.Protocol.Protocol;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode210;
 import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode219;
 import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode406;
 import htw.vs1.filesystem.Network.Protocol.Replies.Reply;
@@ -15,13 +16,12 @@ public class LS extends AbstractCommand {
     public static String COMMAND_STRING = "LS";
 
     public Reply execute(Protocol prot, RequestList requestList) {
-        if(prot.getState().equals(SimpleProtocolState.AUTHENTICATED)) {
-            prot.putLine("YoloSwag Liste");
-        } else {
-            return new SimpleProtocolReply(new ReplyCode406(), this);
-        }
+        if(!prot.getState().equals(SimpleProtocolState.AUTHENTICATED)) return new SimpleProtocolReply(new ReplyCode406(), this);
 
 
+        String ret = prot.getFileSystem().getWorkingDirectory().getContent().toString();
+        new SimpleProtocolReply(new ReplyCode210(), this).putReply(prot);
+        prot.putLine(ret);
 
         return new SimpleProtocolReply(new ReplyCode219(), this);
     }
