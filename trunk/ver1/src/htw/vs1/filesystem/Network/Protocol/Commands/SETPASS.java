83ffbb18e27577649939f4fce4a02bf02efdcbd1
markus <markus@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-06-17 10:20:23 +0000

    SETPASS und SETUSER implementiert

--- trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Commands/SETPASS.java
+++ trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Commands/SETPASS.java
@@ -1,8 +1,14 @@
 package htw.vs1.filesystem.Network.Protocol.Commands;
 
 import htw.vs1.filesystem.Network.Protocol.Protocol;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode100;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode200;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode401;
 import htw.vs1.filesystem.Network.Protocol.Replies.Reply;
+import htw.vs1.filesystem.Network.Protocol.Replies.SimpleProtocolReply;
 import htw.vs1.filesystem.Network.Protocol.Requests.RequestList;
+import htw.vs1.filesystem.Network.Protocol.State.SimpleProtocolState;
 
 /**
  * Created by markus on 12.06.15.
@@ -10,21 +16,39 @@ import htw.vs1.filesystem.Network.Protocol.Requests.RequestList;
 public class SETPASS extends AbstractCommand {
     public static final String COMMAND_STRING = "SETPASS";
 
-
-
     public Reply execute(Protocol prot, RequestList requestList) {
+        if (requestList.getCurrentElement().numOfArguments() != 1) {
+            return new SimpleProtocolReply(new ReplyCode401(COMMAND_STRING+" must have exactly one argument."), this);
+        }
+        String pass = requestList.getCurrentElement().getArguments().get(0);
 
 
-        if (requestList.getPreviousElement().getCommandString().equals(SETUSER.COMMAND_STRING) && requestList.getPreviousElement().hasArguments()) {
-            if (requestList.getCurrentElement().hasArguments()) {
+        //Jetzt muss geprüft werden, ob zuvor SETUSER gesetzt wurde
+        boolean failure = true;
+        try {
+            failure = !SETUSER.isValid(requestList.getPreviousElement());
 
-            } else {
-                prot.putLine("4xx No Arguments supplied");
-            }
-        } else {
-            prot.putLine("4xx SETPASS must be preceeded by valid SETUSER.");
+        } catch (IndexOutOfBoundsException e) {
+            //Nothing to do here. Boobs!
         }
 
-        return null;
+
+        if (failure)
+            return new SimpleProtocolReply(
+                new ReplyCode401(
+                        COMMAND_STRING+ "  must be preceeded by "+SETUSER.COMMAND_STRING),
+                this);
+
+        String user = requestList.getPreviousElement().getArguments().get(0);
+
+        //Username/Passwort korrekt
+
+        if (true) {
+            prot.setState(SimpleProtocolState.AUTHENTICATED);
+        }
+
+        return new SimpleProtocolReply(new ReplyCode100("YO YOO ARE LOGGED IN BIATCH"), this);
+
+
     }
 }
--- trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Commands/SETUSER.java
+++ trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Commands/SETUSER.java
@@ -6,6 +6,7 @@ import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode300;
 import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode401;
 import htw.vs1.filesystem.Network.Protocol.Replies.Reply;
 import htw.vs1.filesystem.Network.Protocol.Replies.SimpleProtocolReply;
+import htw.vs1.filesystem.Network.Protocol.Requests.Request;
 import htw.vs1.filesystem.Network.Protocol.Requests.RequestList;
 import htw.vs1.filesystem.Network.Protocol.State.SimpleProtocolState;
 
@@ -16,18 +17,22 @@ public class SETUSER extends AbstractCommand {
     public static String COMMAND_STRING = "SETUSER";
 
 
+    protected static boolean isValid(Request req) {
+        if (!req.getCommandString().equals(COMMAND_STRING)) return false;
+        if (req.numOfArguments() != 1) return false;
+        return true;
+    }
 
 
     @Override
     public Reply execute(Protocol prot, RequestList requestList) throws SimpleProtocolTerminateConnection {
 
 
-        if (requestList.getCurrentElement().numOfArguments() != 1) {
-            return new SimpleProtocolReply(new ReplyCode401("SETUSER must have exactly one argument."), this);
+        if (!isValid(requestList.getCurrentElement())) {
+            return new SimpleProtocolReply(new ReplyCode401(COMMAND_STRING+" must have exactly one argument."), this);
         }
 
         String user = requestList.getCurrentElement().getArguments().get(0);
-
 
 
         return new SimpleProtocolReply(new ReplyCode300(user), this);
