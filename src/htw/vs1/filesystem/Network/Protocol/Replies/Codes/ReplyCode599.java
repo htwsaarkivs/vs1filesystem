b30068f5c797ad4bb3ee61e78ce7f99ab009e0fa
felixble <felix@blechschmitt.name>
2015-06-21 12:51:29 +0000

    COWBOY 2.0

--- src/htw/vs1/filesystem/Network/Protocol/Replies/Codes/ReplyCode599.java
+++ src/htw/vs1/filesystem/Network/Protocol/Replies/Codes/ReplyCode599.java
@@ -1,23 +1,34 @@
 package htw.vs1.filesystem.Network.Protocol.Replies.Codes;
 
+import htw.vs1.filesystem.Network.Protocol.Replies.Type.SimpleProtocolReplyType;
 import htw.vs1.filesystem.Network.Protocol.Replies.Type.Type;
 
 /**
  * Created by markus on 17.06.15.
  */
 public class ReplyCode599 extends ReplyCode {
+    public static final Type REPLY_TYPE = SimpleProtocolReplyType.CRITICAL_ERROR;
+    public static final int CODE = 599;
+    public static final String DESCRIPTION = "";
+    public static final String STANDARD_MESSAGE = "ILLEGAL COWBOY";
+
+
+    public ReplyCode599() {
+        this.message = STANDARD_MESSAGE;
+    }
+
     @Override
     public int getCode() {
-        return 0;
+        return CODE;
     }
 
     @Override
     public String getDescription() {
-        return null;
+        return DESCRIPTION;
     }
 
     @Override
     public Type getReplyType() {
-        return null;
+        return REPLY_TYPE;
     }
 }
