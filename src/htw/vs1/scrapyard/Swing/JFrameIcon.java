38c95b31eedd9ec128e0eba3142b8169601e62d0
marc <marc@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-06-22 09:39:34 +0000

    Swing erweitert

---
+++ src/htw/vs1/scrapyard/Swing/JFrameIcon.java
@@ -0,0 +1,49 @@
+package htw.vs1.scrapyard.Swing;
+
+import javax.swing.*;
+import java.net.URL;
+
+/**
+ * This class is part of the package htw.vs1.scrapyard.Swing and project ver1
+ * Created by Marc Otting on 22.06.2015.
+ * This class provides the following function(s):
+ */
+public class JFrameIcon extends JFrame {
+    private JPanel panel;
+    private Icon icon1;
+    private Icon icon2;
+
+    public void start() {
+        buildGUI();
+    }
+
+    private void buildGUI() {
+        this.setSize(250,250);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        buildPane();
+        this.add(panel);
+        this.pack();
+
+    }
+
+    private void buildPane() {
+        panel = new JPanel();
+
+
+        URL imgURL = getClass().getResource("\\SwingIcons\\56x56_Haken_Gruen.png");
+        URL imgURL2 = getClass().getResource("\\SwingIcons\\56x56_Haken_Rot.png");
+
+        icon1 = new ImageIcon(imgURL);
+        icon2 = new ImageIcon(imgURL2);
+
+        JButton button1 = new JButton(icon2);
+        JButton button2 = new JButton(icon1);
+
+        panel.add(button1);
+        panel.add(button2);
+
+
+    }
+}
---
+++ src/htw/vs1/scrapyard/Swing/startGUI.java
@@ -0,0 +1,19 @@
+package htw.vs1.scrapyard.Swing;
+
+/**
+ * This class is part of the package com.company.awt and project test
+ * Created by Marc Otting on 15.06.2015.
+ * This class provides the following function(s):
+ */
+public class startGUI {
+    public static void main(String[] args) {
+//        SwingGUIPrinzipNoContainer fenster = new SwingGUIPrinzipNoContainer();
+//        fenster.start();
+
+//        JLabelGUI gui = new JLabelGUI();
+//        gui.start();
+
+        JFrameIcon frameIcon = new JFrameIcon();
+        frameIcon.start();
+    }
+}
