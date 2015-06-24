6d757cf7becbf63fa1f1854d05f08e33e7a97b27
hendrik <hendrik@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-06-24 15:26:36 +0000

    getfeat implementiert

--- trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Commands/GETFEAT.java
+++ trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Commands/GETFEAT.java
@@ -2,12 +2,12 @@ package htw.vs1.filesystem.Network.Protocol.Commands;
 
 
 import htw.vs1.filesystem.Network.Protocol.Protocol;
-import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode200;
 import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode210;
 import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode219;
 import htw.vs1.filesystem.Network.Protocol.Replies.Reply;
 import htw.vs1.filesystem.Network.Protocol.Replies.SimpleProtocolReply;
 import htw.vs1.filesystem.Network.Protocol.Requests.RequestList;
+import java.util.Set;
 
 /**
  * Created by markus on 12.06.15.
@@ -17,15 +17,19 @@ public class GETFEAT extends AbstractCommand {
 
 
     public Reply execute(Protocol prot, RequestList requestList) {
-        prot.putLine("This Server supports amazingly many features");
-        prot.putLine("Current State: "+prot.getState().toString());
+
+        Set<String> set = new CommandFactory().getRegisteredCommands();
+
+        StringBuffer buf = new StringBuffer();
+
+        for (String obj : set) {
+            buf.append("[" + obj + "]\n");
+        }
 
         new SimpleProtocolReply(new ReplyCode210(), this).putReply(prot);
 
-        prot.putLine(new CommandFactory().getRegisteredCommands().toString());
+        prot.putLine(buf.toString());
 
         return new SimpleProtocolReply(new ReplyCode219(), this);
     }
-
-
 }
