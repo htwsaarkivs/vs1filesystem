817912892ada54f75e8b6a3356a3ce3588b9042e
marc <marc@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-06-25 14:08:41 +0000

    Exeptions update

---
+++ trunk/ver1/src/htw/vs1/filesystem/FileSystem/exceptions/CouldNotCreateException.java
@@ -0,0 +1,40 @@
+package htw.vs1.filesystem.FileSystem.exceptions;
+
+import htw.vs1.filesystem.FileSystem.virtual.FSObject;
+
+/**
+ * This class is part of the package htw.vs1.filesystem.FileSystem.exceptions and project ver1
+ * Created by Marc Otting on 14.06.2015.
+ * This class provides the following function(s):
+ */
+public class CouldNotCreateException extends FSObjectException {
+    /**
+     * Costructor of a Exeption with 3 Parameters
+     *
+     * @param object current Object the Exeption is regarding to
+     * @param msg    Error message of the Expetion
+     * @param ex     Exeption object to deal with
+     */
+    public CouldNotCreateException(FSObject object, String msg, Throwable ex) {
+        super(object, msg, ex);
+    }
+
+    /**
+     * Costructor of a Exeption with 2 Parameters
+     *
+     * @param msg Error message of the Expetion
+     * @param ex  Exeption object to deal wit
+     */
+    public CouldNotCreateException(String msg, Throwable ex) {
+        super(msg, ex);
+    }
+
+    /**
+     * Costructor of a Exeption with 1 Parameter
+     *
+     * @param msg Error message of the Expetion
+     */
+    public CouldNotCreateException(String msg) {
+        super(msg);
+    }
+}
