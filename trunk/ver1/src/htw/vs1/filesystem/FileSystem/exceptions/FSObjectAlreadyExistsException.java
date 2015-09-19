921ba28df2fd9ccfee1af55f384dea8fe3912d67
felix <felix@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-09-19 15:06:58 +0000

    Alle Exception nehmen jetzt auch nur 1 arg - die message.

--- trunk/ver1/src/htw/vs1/filesystem/FileSystem/exceptions/FSObjectAlreadyExistsException.java
+++ trunk/ver1/src/htw/vs1/filesystem/FileSystem/exceptions/FSObjectAlreadyExistsException.java
@@ -1,6 +1,8 @@
 package htw.vs1.filesystem.FileSystem.exceptions;
 
 import htw.vs1.filesystem.FileSystem.virtual.FSObject;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode404;
 
 /**
  * Created by Felix on 19.09.2015.
@@ -16,5 +18,12 @@ public class FSObjectAlreadyExistsException extends FSObjectException {
 
     public FSObjectAlreadyExistsException(String msg) {
         super(msg);
+    }
+
+    @Override
+    public ReplyCode getReplyCode() {
+        ReplyCode code = new ReplyCode404();
+        code.setAdditionalMessage(getMessage());
+        return code;
     }
 }
--- trunk/ver1/src/htw/vs1/filesystem/FileSystem/exceptions/InvalidFilenameException.java
+++ trunk/ver1/src/htw/vs1/filesystem/FileSystem/exceptions/InvalidFilenameException.java
@@ -1,6 +1,8 @@
 package htw.vs1.filesystem.FileSystem.exceptions;
 
 import htw.vs1.filesystem.FileSystem.virtual.FSObject;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode409;
 
 /**
  * Created by Adrian on 19.06.2015.
@@ -18,5 +20,12 @@ public class InvalidFilenameException extends FSObjectException{
 
     public InvalidFilenameException(String msg) {
         super(msg);
+    }
+
+    @Override
+    public ReplyCode getReplyCode() {
+        ReplyCode code = new ReplyCode409();
+        code.setAdditionalMessage(getMessage());
+        return code;
     }
 }
