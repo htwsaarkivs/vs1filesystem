bfbc7d465fc309ac9ec619b62def7dfa8edb76dc
markus <markus@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-09-19 13:38:40 +0000

    Party Bitches und Search Command.

--- trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Commands/SEARCH.java
+++ trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/Commands/SEARCH.java
@@ -1,7 +1,49 @@
 package htw.vs1.filesystem.Network.Protocol.Commands;
 
+import htw.vs1.filesystem.Network.Protocol.Client.ClientProtocol;
+import htw.vs1.filesystem.Network.Protocol.Exceptions.SimpleProtocolTerminateConnection;
+import htw.vs1.filesystem.Network.Protocol.Replies.ClientReply;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode219;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode401;
+import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode406;
+import htw.vs1.filesystem.Network.Protocol.Replies.ServerReply;
+import htw.vs1.filesystem.Network.Protocol.Replies.SimpleServerProtocolReply;
+import htw.vs1.filesystem.Network.Protocol.Requests.RequestList;
+import htw.vs1.filesystem.Network.Protocol.Server.ServerProtocol;
+import htw.vs1.filesystem.Network.Protocol.State.SimpleProtocolState;
+
 /**
  * Created by markus on 06.09.15.
  */
-public class SEARCH {
+public class SEARCH extends AbstractCommand {
+
+
+    public static String COMMAND_STRING = "SEARCH";
+
+    @Override
+    public ServerReply execute(ServerProtocol prot, RequestList requestList) throws SimpleProtocolTerminateConnection {
+        if (prot.getState() != SimpleProtocolState.AUTHENTICATED)
+            return new SimpleServerProtocolReply(new ReplyCode406(), this);
+
+        if (requestList.getCurrentElement().numOfArguments() != 1)
+            return new SimpleServerProtocolReply(
+                    new ReplyCode401(COMMAND_STRING + " must have exactly two arguments"),
+                    this);
+
+        prot.getFileSystem().search(
+                requestList
+                        .getCurrentElement()
+                        .getArguments()
+                        .get(0)
+        );
+
+
+        return new SimpleServerProtocolReply(new ReplyCode219(), this);
+
+    }
+
+    @Override
+    public ClientReply invoke(ClientProtocol prot, String... parameters) throws SimpleProtocolTerminateConnection {
+        return null;
+    }
 }
