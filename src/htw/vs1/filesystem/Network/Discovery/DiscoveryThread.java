3c2f3dbd941686baa093d9a6f9c4c61ee991d1a9
felixble <felix@blechschmitt.name>
2015-09-20 16:58:11 +0000

    You and me we are just mammals and we do it like they do it on the discovery channel düdüdüdedededöde

---
+++ src/htw/vs1/filesystem/Network/Discovery/DiscoveryThread.java
@@ -0,0 +1,41 @@
+package htw.vs1.filesystem.Network.Discovery;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+/**
+ * Created by Felix on 20.09.2015.
+ */
+public abstract class DiscoveryThread extends Thread {
+
+    protected volatile boolean running = true;
+
+    private DatagramSocket socket;
+
+    protected abstract void discovery(DatagramSocket socket) throws InterruptedException;
+
+    protected abstract DatagramSocket getDatagramSocket() throws SocketException;
+
+    @Override
+    public void run() {
+        try {
+            socket = getDatagramSocket();
+
+            while (running) {
+                try {
+                    discovery(socket);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    running = false;
+                }
+            }
+
+            if (null != socket) {
+                socket.close();
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+}
