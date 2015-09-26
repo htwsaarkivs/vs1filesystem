982209d7c056031ecba0d2d95e0c62518116dd6f
felix <felix@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-09-26 16:59:43 +0000

    GUI Pfad über Parameter.

--- trunk/ver1/src/htw/vs1/filesystem/GUI/Main.java
+++ trunk/ver1/src/htw/vs1/filesystem/GUI/Main.java
@@ -1,6 +1,8 @@
 package htw.vs1.filesystem.GUI;
 
 import htw.vs1.filesystem.FileSystem.physical.PhysicalFileSystemAdapter;
+import htw.vs1.filesystem.FileSystemManger;
+import htw.vs1.filesystem.Network.TCPParallelServer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +15,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Virtual Filesystem");
         primaryStage.setScene(new Scene(root, 800, 600));
@@ -20,13 +24,18 @@ public class Main extends Application {
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent event) {
-                PhysicalFileSystemAdapter.getInstance().stopWatchService();
+                FileSystemManger.getInstance().close();
             }
         });
     }
 
 
     public static void main(String[] args) {
+        if (args.length != 1) {
+            htw.vs1.filesystem.Main.usage();
+        }
+        FileSystemManger.getInstance().init(args[0], TCPParallelServer.DEFAULT_PORT);
+
         launch(args);
     }
 }
