dbfb4205fe67ad4651e6c6ade9746dbd414d4706
hendrik <hendrik@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-09-29 13:48:57 +0000

    Rechtschreibfehler in Kommentaren entfernt

--- trunk/ver1/src/htw/vs1/filesystem/FileSystem/exceptions/CouldNotCreateException.java
+++ trunk/ver1/src/htw/vs1/filesystem/FileSystem/exceptions/CouldNotCreateException.java
@@ -11,30 +11,30 @@ import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode408;
  */
 public class CouldNotCreateException extends FSObjectException {
     /**
-     * Costructor of a Exeption with 3 Parameters
+     * Constructor of a Exception with 3 Parameters
      *
-     * @param object current Object the Exeption is regarding to
-     * @param msg    Error message of the Expetion
-     * @param ex     Exeption object to deal with
+     * @param object current Object the Exception is regarding to
+     * @param msg    Error message of the Exception
+     * @param ex     Exception object to deal with
      */
     public CouldNotCreateException(FSObject object, String msg, Throwable ex) {
         super(object, msg, ex);
     }
 
     /**
-     * Costructor of a Exeption with 2 Parameters
+     * Constructor of a Exception with 2 Parameters
      *
-     * @param msg Error message of the Expetion
-     * @param ex  Exeption object to deal wit
+     * @param msg Error message of the Exception
+     * @param ex  Exception object to deal wit
      */
     public CouldNotCreateException(String msg, Throwable ex) {
         super(msg, ex);
     }
 
     /**
-     * Costructor of a Exeption with 1 Parameter
+     * Constructor of a Exception with 1 Parameter
      *
-     * @param msg Error message of the Expetion
+     * @param msg Error message of the Exception
      */
     public CouldNotCreateException(String msg) {
         super(msg);
