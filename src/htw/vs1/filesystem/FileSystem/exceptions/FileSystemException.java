38713276a6f436302095bc37ec92b6c5615626c5
hendrik <hendrik@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-09-29 13:52:38 +0000

    Rechtschreibfehler in Kommentaren entfernt

--- src/htw/vs1/filesystem/FileSystem/exceptions/FileSystemException.java
+++ src/htw/vs1/filesystem/FileSystem/exceptions/FileSystemException.java
@@ -6,22 +6,22 @@ import htw.vs1.filesystem.Network.Protocol.Replies.Codes.ReplyCode;
  * This class is part of the package htw.vs1.filesystem.FileSystem.exceptions and project ver1
  * Created by Marc Otting on 14.06.2015.
  * This class provides the following function(s): FileSystemException is the super class
- * in the Exeptionhirarchy of our filesystem
+ * in the Exception hierarchy of our filesystem
  */
 public abstract class FileSystemException extends Exception {
 
     /**
-     * Costructor of a Exeption with 2 Parameters
-     * @param msg Error message of the Expetion
-     * @param reason Exeption object to deal with
+     * Constructor of a Exception with 2 Parameters
+     * @param msg Error message of the Exception
+     * @param reason Exception object to deal with
      */
     public FileSystemException(String msg, Throwable reason){
         super(msg, reason);
     }
 
     /**
-     * Costructor of a Exeption with 1 Parameter
-     * @param msg Error message of the Expetion
+     * Constructor of a Exception with 1 Parameter
+     * @param msg Error message of the Exception
      */
     public FileSystemException(String msg){
         super(msg);
