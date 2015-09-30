efd98e3adb4b4a928767144e24ce170507ecd519
felix <felix@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-09-30 20:42:10 +0000

    server status indicator.

---
+++ trunk/ver1/src/htw/vs1/filesystem/GUI/Resources.java
@@ -0,0 +1,17 @@
+package htw.vs1.filesystem.GUI;
+
+import javafx.scene.image.Image;
+
+/**
+ * Created by Felix on 30.09.2015.
+ */
+public class Resources {
+
+    public static final Image server_status_indicator_red
+            = new Image(FileType.class.getResource("images/Button_Red/Button_Red_032.png").toString());
+
+
+    public static final Image server_status_indicator_green
+            = new Image(FileType.class.getResource("images/Button_Green/Button_Green_032.png").toString());
+
+}
---
+++ trunk/ver1/src/htw/vs1/filesystem/Network/Protocol/ServerStatus.java
@@ -0,0 +1,10 @@
+package htw.vs1.filesystem.Network.Protocol;
+
+/**
+ * Created by Felix on 30.09.2015.
+ */
+public enum ServerStatus {
+    RUNNING,
+    STOPPED,
+    ERROR
+}
