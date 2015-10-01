8fea9aef8058a8f2af8ec5f14b9a5d1b351514f0
markus228 <markus228@users.noreply.github.com>
2015-10-01 20:39:20 +0000

    ThreadPool hinzugefügt, um Anzahl gleichzeitiger Threads regulieren zu können. Beenden des Servers ermöglicht.

--- src/htw/vs1/filesystem/Network/TCPParallelWorker.java
+++ src/htw/vs1/filesystem/Network/TCPParallelWorker.java
@@ -3,21 +3,33 @@ package htw.vs1.filesystem.Network;
 import htw.vs1.filesystem.FileSystem.virtual.FileSystem;
 import htw.vs1.filesystem.FileSystemManger;
 import htw.vs1.filesystem.Network.Protocol.Server.SimpleServerProtocol;
+import jdk.net.Sockets;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.util.logging.SocketHandler;
 
 /**
  * Created by markus on 07.06.15.
  */
-public class TCPParallelWorker extends Thread {
+public class TCPParallelWorker implements Runnable {
 
 
     private static final String THREAD_NAME = "WorkerThread";
     private Socket socket;
 
     public TCPParallelWorker(Socket socket) {
-        setName(THREAD_NAME);
+        //setName(THREAD_NAME);
         this.socket = socket;
+    }
+
+
+    public void stopWorker() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
