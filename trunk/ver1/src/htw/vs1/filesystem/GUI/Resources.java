28db1d74604cff79eba40a50ad54180ea367f3de
felix <felix@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-10-03 14:06:34 +0000

    Lock für files.

--- trunk/ver1/src/htw/vs1/filesystem/GUI/Resources.java
+++ trunk/ver1/src/htw/vs1/filesystem/GUI/Resources.java
@@ -15,15 +15,15 @@ public class Resources {
     public static final Image server_status_indicator_green
             = new Image(FileType.class.getResource("images/Button_Green/Button_Green_032.png").toString());
 
-    public static final Image fsObjectIcon(boolean isFolder, Permissions permissions) {
+    public static Image fsObjectIcon(boolean isFolder, Permissions permissions) {
         if (isFolder) {
             return (permissions.isLocked())
                     ? new Image(FileType.class.getResource("images/Folder_Lock/Folder_Lock_032.png").toString())
                     : new Image(FileType.class.getResource("images/Folder/Folder_032.png").toString());
         } else {
             return (permissions.isLocked())
-                    ? new Image(FileType.class.getResource("images/File/File_032.png").toString())
-                    : new Image(FileType.class.getResource("images/File/File_032.png").toString());
+                    ? new Image(FileType.class.getResource("images/File_Lock/File_Lock_032.png").toString())
+                    : new Image(FileType.class.getResource("images/File_Text/File_Text_032.png").toString());
         }
     }
 
