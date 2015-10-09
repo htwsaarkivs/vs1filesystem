9b400c833103110dfbe27b38382c491c6b852e90
marc <marc@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-10-09 15:27:19 +0000

    implementierung iconfactory für searchview

--- src/htw/vs1/filesystem/GUI/SearchItem.java
+++ src/htw/vs1/filesystem/GUI/SearchItem.java
@@ -1,6 +1,9 @@
 package htw.vs1.filesystem.GUI;
 
+import htw.vs1.filesystem.FileSystem.virtual.Permissions;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.image.Image;
 
 /**
  * This class is part of the package htw.vs1.filesystem.FileSystem.GUI and project ver1
@@ -9,15 +12,27 @@ import javafx.beans.property.SimpleStringProperty;
  */
 public class SearchItem {
 
-    public static final int COL_NAME_INDEX = 0;
-    public static final int COL_DIR_INDEX = 1;
-
+    private final SimpleObjectProperty<Image> icon;
     private final SimpleStringProperty fileName;
     private final SimpleStringProperty path;
 
-    public SearchItem(String filename,String path){
-        this.fileName = new SimpleStringProperty(filename);
+
+    public SearchItem(String fileName, boolean isFolder, Permissions permissions, String path){
+        this.fileName = new SimpleStringProperty(fileName);
         this.path = new SimpleStringProperty(path);
+        this.icon = new SimpleObjectProperty<>(Resources.fsObjectIcon(isFolder, permissions));
+    }
+
+    public Image getIcon() {
+        return icon.get();
+    }
+
+    public SimpleObjectProperty<Image> iconProperty() {
+        return icon;
+    }
+
+    public void setIcon(Image icon) {
+        this.icon.set(icon);
     }
 
     public String getFileName() {
@@ -34,5 +49,9 @@ public class SearchItem {
 
     public SimpleStringProperty pathProperty() {
         return path;
+    }
+
+    public void setPath(String path) {
+        this.path.set(path);
     }
 }
