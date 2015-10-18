5445e326a2d8f4318771c0bb2c4cbf0d22819d02
felix <felix@05103a78-b414-4ce6-94ee-b0b05ba25bd7>
2015-10-18 14:20:53 +0000

    GUI Help pdf öffnen aus der jar.

---
+++ trunk/ver1/src/htw/vs1/filesystem/GUI/Tools.java
@@ -0,0 +1,35 @@
+package htw.vs1.filesystem.GUI;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by Felix on 18.10.2015.
+ */
+public class Tools {
+
+    static void copyStream( InputStream inputStream, OutputStream outputStream ) throws IOException {
+        try
+        {
+            byte  buffer[] = new byte[0xffff];
+            int   numBytes;
+
+            while ( (numBytes = inputStream.read(buffer)) != -1 ) {
+                outputStream.write(buffer, 0, numBytes);
+            }
+        }
+        finally {
+            if ( inputStream != null )
+                try {
+                    inputStream.close();
+                } catch ( IOException e ) {}
+
+            try {
+                if ( outputStream != null )
+                    outputStream.close();
+            } catch ( IOException e ) {}
+        }
+    }
+
+}
