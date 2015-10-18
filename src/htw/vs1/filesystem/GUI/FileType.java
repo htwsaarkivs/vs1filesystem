f6fbe95c70252c7deda4d8f62dca5870c018f509
felixble <felix@blechschmitt.name>
2015-10-18 14:04:49 +0000

    Nach doppelklick auf Suchelement wird dieses nun auch markiert ;-)

--- src/htw/vs1/filesystem/GUI/FileType.java
+++ src/htw/vs1/filesystem/GUI/FileType.java
@@ -5,6 +5,8 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.image.Image;
 
+import java.util.Objects;
+
 /**
  * This class is part of the package htw.vs1.filesystem.FileSystem.GUI and project ver1
  * Created by Marc Otting on 18.09.2015.
@@ -20,7 +22,10 @@ public class FileType {
 
     private Permissions permissions;
 
+    private String fileNameString;
+
     public FileType (String fileName, boolean isFolder, Permissions permissions) {
+        this.fileNameString = fileName;
         this.permissions = permissions;
         this.fileName = new SimpleObjectProperty<>(new FileObject(fileName, isFolder));
         this.fileType = new SimpleStringProperty(isFolder ? "Folder" : "File");
@@ -54,5 +59,15 @@ public class FileType {
 
     public Permissions getPermissions() {
         return permissions;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof FileType) {
+            // two FileType-Objects are equal if they have the same fileNameString.
+            return Objects.equals(((FileType) obj).fileNameString, fileNameString);
+        }
+
+        return super.equals(obj);
     }
 }
